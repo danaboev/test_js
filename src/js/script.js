05f95570600c3c6220f8efbe449f4c57f73a98c0
jahon <danaboev.jf@gamil.com>
2026-09-22 12:17:37 +0500

    first commit

---
+++ src/js/script.js
@@ -0,0 +1,28 @@
+let a = 10; 
+console.log(a);
+
+a = 20;
+console.log(a);
+
+let leftDoor = "red ";
+console.log(leftDoor);
+
+
+
+const rightDoor = "blue ";
+console.log(rightDoor);
+
+rightDoor = "green ";
+console.log(rightDoor);
+
+const user = {
+	name: "John",
+  	age: 30,
+  	isAdmin: true
+};
+console.log(user);
+
+console.log(user.name);
+console.log(user.age);
+console.log(user.isAdmin);
+
